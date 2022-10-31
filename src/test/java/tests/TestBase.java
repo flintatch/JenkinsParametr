f83db66a40e7ae81a3025895d135bf4aea87a663
gvlad <gvladovsky@gmail.com>
2022-10-31 23:00:22 +0300

    added TestBase Condition

--- src/test/java/tests/TestBase.java
+++ src/test/java/tests/TestBase.java
@@ -6,6 +6,8 @@ import com.codeborne.selenide.logevents.SelenideLogger;
 import io.qameta.allure.selenide.AllureSelenide;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Tag;
+import org.junit.jupiter.api.Test;
 import org.openqa.selenium.remote.DesiredCapabilities;
 
 
@@ -16,9 +18,10 @@ public class TestBase {
         SelenideLogger.addListener("AllureSelenide", new AllureSelenide());
 
         Configuration.baseUrl = "https://demoqa.com";
-        Configuration.browserSize = "1920x1080";
-        Configuration.holdBrowserOpen = true;
-        Configuration.remote = "https://user1:1234@selenoid.autotests.cloud/wd/hub";
+        String browserName = System.getProperty("browser", "edge");
+        String browserVersion = System.getProperty("browser_version", "100");
+        String browserSize = System.getProperty("browser_size", "1920x1080");
+        String remoteUrl = System.getProperty("remote_Url", "https://user1:1234@selenoid.autotests.cloud/wd/hub");
 
         if (System.getProperty("remote_driver") != null) {
 
